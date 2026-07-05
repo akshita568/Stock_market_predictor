323710642405a422020be14b3f9299649171cef1
Akshita Sharma <sakshita229@gmail.com>
2026-07-06 01:30:48 +0530

    data cleaning

--- download_data.py
+++ download_data.py
@@ -1,10 +1,10 @@
 import os
+import pandas as pd
 import yfinance as yf
 
-# Create data folder if it doesn't exist
+# Create data folder
 os.makedirs("data", exist_ok=True)
 
-# List of stock symbols
 stocks = {
     "AAPL": "Apple",
     "MSFT": "Microsoft",
@@ -19,6 +19,7 @@ stocks = {
 }
 
 for ticker, company in stocks.items():
+
     print(f"Downloading {company} ({ticker})...")
 
     df = yf.download(
@@ -26,11 +27,23 @@ for ticker, company in stocks.items():
         start="2015-01-01",
         end="2025-01-01",
         auto_adjust=False,
-        progress=False
+        progress=False,
+        multi_level_index=False
     )
 
+    # Flatten columns if your yfinance version still returns MultiIndex
+    if isinstance(df.columns, pd.MultiIndex):
+        df.columns = df.columns.get_level_values(0)
+
+    # Make Date a normal column
+    df = df.reset_index()
+
+    # Add company symbol
+    df["Symbol"] = ticker
+
+    # Save
     filename = f"data/{ticker.replace('.', '_')}.csv"
-    df.to_csv(filename)
+    df.to_csv(filename, index=False)
 
     print(f"Saved -> {filename}")
 
